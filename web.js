3e28ba3da10a5e8a58b6a2521be76ef33eec3b85
Malvin Tan <malvintanyeephang@gmail.com>
2013-07-08 10:15:26 +0000

    edited web.js

--- web.js
+++ web.js
@@ -1,14 +1,14 @@
-var express = require('express');
+AOAvar express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 
     var fs = require("fs");
-    var buffer = new Buffer();
-    var data = fs.readFileSync("index.html", "utf-8");
-      
-    response.send(data);
+    var buffer = new Buffer("utf-8", 25);
+    fs.readFileSync("index.html", "utf-8");
+
+    response.send(buffer.toString(fs.readFileSync));
 });
 
 var port = process.env.PORT || 5000;
